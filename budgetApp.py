25664968e36196974cdc928c1fc189a03a546dcb
KDS-Limitless <limitlesskds@gmail.com>
2021-04-14 17:23:40 +0100

    Fix: Added Error Checks/bug fixes

--- budgetApp.py
+++ budgetApp.py
@@ -1,5 +1,5 @@
 import json
-import os
+from os import error
 
 
 class Budget:
@@ -7,34 +7,35 @@ class Budget:
     db = {}
 
 
-    def __init__(self):
-        pass
-
     def deposit_funds(self, category: str):
-        print(os.getcwd())
         amount = int(input(f"How much do you want to assing for {category} \n >>>"))
         if category == 'food'.lower():
             self.db['FoodBudget'] = {'amount': amount, 'balance': 0}
-            with open('zuriPurse.txt') as file:
-                data = json.load(file)
-                
-                with open('zuriPurse.txt', 'w') as dbFile:
-                    if data['FoodBudget']['balance'] == 0:
-                        data['FoodBudget']['balance'] += amount
-                        data['FoodBudget']['amount'] = self.db['FoodBudget']['amount']
-                        print(self.db)
-                        json.dump(data, dbFile)
+            try:
+                with open('zuriPurse.txt') as file:
+                    data = json.load(file)
+            except error:
+                with open('zuriPurse.txt', 'w') as dbdata:
+                    data = {}
+                    data['FoodBudget'] = {'amount': amount, 'balance': 0}
+                    json.dump(data, dbdata)
 
-                    else:
-                        data['FoodBudget']['balance'] += self.db['FoodBudget']['amount']
-                        data['FoodBudget']['amount'] = self.db['FoodBudget']['amount']
-                        print(self.db)
-                        json.dump(data, dbFile)
-                    
-        print(
-            f"{self.naira}{amount} added to {category} budget. \n",\
-                end=f'Your {category} budget balance is ' + str(data['FoodBudget']['balance'])
-            )
+            with open('zuriPurse.txt', 'w') as dbFile:
+                if data['FoodBudget']['balance'] == 0:
+                    data['FoodBudget']['balance'] += amount
+                    data['FoodBudget']['amount'] = self.db['FoodBudget']['amount']
+                    print(self.db)
+                    json.dump(data, dbFile)
+                else:
+                    data['FoodBudget']['balance'] += self.db['FoodBudget']['amount']
+                    data['FoodBudget']['amount'] = self.db['FoodBudget']['amount']
+                    print(self.db)
+                    json.dump(data, dbFile)
+
+                print(
+                    f"{self.naira}{amount} added to {category} budget. \n",\
+                        end=f'Your {category} budget balance is {self.naira}' + str(data['FoodBudget']['balance'])
+                    )
 
                     
 
