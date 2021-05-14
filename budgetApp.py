5322b8486d14d791f270d1916c7ca3fd8954bd8f
KDS-Limitless <limitlesskds@gmail.com>
2021-05-14 19:42:11 +0100

    Refactored deposit_funds function/ Bug Fix

--- budgetApp.py
+++ budgetApp.py
@@ -11,88 +11,6 @@ class Budget:
         self.deposit_funds(category="entertainment")
         self.deposit_funds(category="food")
 
-    def deposit_funds(self, category=""):
-        last_deposit_amount = int(input(f"\n \n How much do you want to assign for {category} \n >>> {self.naira}"))
-        if category == 'food'.lower():
-            self.db['food'] = {'last_deposit_amount': last_deposit_amount, 'balance': 0}
-            try:
-                with open('FoodPurse.txt') as file:  # read file
-                    data = json.load(file)
-                    if not data['food']:
-                        pass
-            except (json.decoder.JSONDecodeError, FileNotFoundError, KeyError):
-                with open('FoodPurse.txt', 'w') as dbdata: # create data if not exists
-                    data = {}
-                    data['food'] = {'last_deposit_amount': last_deposit_amount, 'balance': 0}
-                    json.dump(data, dbdata, indent=2)
-
-            with open('FoodPurse.txt', 'w') as dbFile: # Save balance to file
-                if data['food']['balance'] == 0:
-                    data['food']['balance'] += last_deposit_amount
-                    data['food']['last_deposit_amount'] = self.db['food']['last_deposit_amount']
-                    json.dump(data, dbFile, indent=2)
-                else:
-                    data['food']['balance'] += self.db['food']['last_deposit_amount']
-                    data['food']['last_deposit_amount'] = self.db['food']['last_deposit_amount']
-                    json.dump(data, dbFile, indent=2)
-                print(
-                    f"{self.naira}{last_deposit_amount} added to {category} budget. \n",\
-                        end=f'Your {category} budget balance is {self.naira}' + str(data[category]['balance'])
-                    )
-
-        elif category == 'clothing'.lower():
-            self.db['clothing'] = {'last_deposit_amount': last_deposit_amount, 'balance': 0}
-            try:
-                with open('clothingPurse.txt') as file: # read file
-                    data = json.load(file)
-                    if not data['clothing']:
-                        pass
-            except (json.decoder.JSONDecodeError, FileNotFoundError, KeyError):
-                with open('clothingPurse.txt', 'w') as dbdata: # create data if not exists
-                    data = {}
-                    data['clothing'] = {'last_deposit_amount': last_deposit_amount, 'balance': 0}
-                    json.dump(data, dbdata, indent=2)
-
-            with open('clothingPurse.txt', 'w') as dbFile: # Save balance to file
-                if data['clothing']['balance'] == 0:
-                    data['clothing']['balance'] += last_deposit_amount
-                    data['clothing']['last_deposit_amount'] = self.db['clothing']['last_deposit_amount']
-                    json.dump(data, dbFile, indent=2)
-                else:
-                    data['clothing']['balance'] += self.db['clothing']['last_deposit_amount']
-                    data['clothing']['last_deposit_amount'] = self.db['clothing']['last_deposit_amount']
-                    json.dump(data, dbFile, indent=2)
-                print(
-                    f"{self.naira}{last_deposit_amount} added to {category} budget. \n",\
-                        end=f'Your {category} budget balance is {self.naira}' + str(data[category]['balance'])
-                    )
-        elif category == category == 'entertainment'.lower():
-            self.db['entertainment'] = {'last_deposit_amount': last_deposit_amount, 'balance': 0}
-            try:
-                with open('entertainmentPurse.txt') as file: # read file
-                    data = json.load(file)
-                    if not data['entertainment']:
-                        pass
-            except (json.decoder.JSONDecodeError, FileNotFoundError, KeyError):
-                with open('entertainmentPurse.txt', 'w') as dbdata: # create data if not exists
-                    data = {}
-                    data['entertainment'] = {'last_deposit_amount': last_deposit_amount, 'balance': 0}
-                    json.dump(data, dbdata, indent=2)
-
-            with open('entertainmentPurse.txt', 'w') as dbFile: # Save balance to file
-                if data['entertainment']['balance'] == 0:
-                    data['entertainment']['balance'] += last_deposit_amount
-                    data['entertainment']['last_deposit_amount'] = self.db['entertainment']['last_deposit_amount']
-                    json.dump(data, dbFile, indent=2)
-                else:
-                    data['entertainment']['balance'] += self.db['entertainment']['last_deposit_amount']
-                    data['entertainment']['last_deposit_amount'] = self.db['entertainment']['last_deposit_amount']
-                    json.dump(data, dbFile, indent=2)
-                print(
-                    f"{self.naira}{last_deposit_amount} added to {category} budget. \n",\
-                        end=f'Your {category} budget balance is {self.naira}' + str(data[category]['balance'])
-                    )
-
     def read_file(self, category):
         if category == 'food':
             with open('FoodPurse.txt') as file:
@@ -120,6 +38,37 @@ class Budget:
             with open('entertainmentPurse.txt', 'w') as file:
                 json.dump(self.Purse, file, indent=2)
 
+    def deposit_funds(self, category=""):
+        last_deposit_amount = int(input(f"\n \n How much do you want to assign for {category} \n >>> {self.naira}"))
+        self.db[category] = {'last_deposit_amount': last_deposit_amount, 'balance': 0}
+
+        try:
+            self.read_file(category)
+          
+        except (json.decoder.JSONDecodeError, FileNotFoundError, KeyError):
+            self.Purse = {}
+            self.Purse[category] = {'last_deposit_amount': last_deposit_amount, 'balance': 0}
+            self.write_to_file(category)
+
+        if self.Purse[category]['balance'] == 0:
+            self.Purse[category]['balance'] += last_deposit_amount
+            self.Purse[category]['last_deposit_amount'] = self.db[category]['last_deposit_amount']
+            self.write_to_file(category)
+
+            print(
+                f"{self.naira}{last_deposit_amount} added to {category} budget. \n",\
+                    end=f'Your {category} budget balance is {self.naira}' + str(self.Purse[category]['balance'])
+                )
+        else:
+            self.Purse[category]['balance'] += self.db[category]['last_deposit_amount']
+            self.Purse[category]['last_deposit_amount'] = last_deposit_amount
+            self.write_to_file(category)
+
+            print(
+                f"{self.naira}{last_deposit_amount} added to {category} budget. \n",\
+                    end=f'Your {category} budget balance is {self.naira}' + str(self.Purse[category]['balance'])
+                )
+
     def withdraw_funds(self, category="", amount=0):
         print()
         print(f'\n Withdraw {self.naira}{amount} from {category} balance?')
@@ -140,47 +89,7 @@ class Budget:
             sys.exit()
         else:
             print("invalid Command!")
-            withdraw_funds(category=category, amount=amount)
-
-
-        # if confirm == 1 and category == 'food':
-        #    with open('FoodPurse.txt') as file:
-        #        foodPurse = json.load(file)
-        #        if foodPurse[category]['balance'] < amount:
-        #            print(f"Insuficient balance in {category} budget")
-        #            sys.exit()
-        #        with open('FoodPurse.txt', "w") as file:
-        #            foodPurse[category]['balance'] -= amount
-        #            json.dump(foodPurse, file, indent=2)
-        #            print(f"{amount} dedducted from your {category} budget balance. \n")
-        #
-        # elif confirm == 1 and category == 'clothing':
-        #    with open('clothingPurse.txt') as file:
-        #        clothingPurse = json.load(file)
-        #        if clothingPurse[category]['balance'] < amount:
-        #            print(f"Insuficient balance in {category} budget")
-        #            sys.exit()
-        #        with open('clothingPurse.txt', "w") as file:
-        #            clothingPurse[category]['balance'] -= amount
-        #            json.dump(clothingPurse, file, indent=2)
-        #            print(f"{amount} dedducted from your {category} budget balance. \n")
-#
-        # elif confirm == 1 and category == 'entertainment':
-        #    with open('entertainmentPurse.txt') as file:
-        #        entertainmentPurse = json.load(file)
-        #        if entertainmentPurse[category]['balance'] < amount:
-        #            print(f"Insuficient balance in {category} budget")
-        #            sys.exit()
-        #        with open('entertainmentPurse.txt', "w") as file:
-        #            entertainmentPurse[category]['balance'] -= amount
-        #            json.dump(entertainmentPurse, file, indent=2)
-        #            print(f"{amount} dedducted from your {category} budget balance. \n")
-#
-        # elif confirm == 2:
-        #    print("Have a nice day!")
-        #    sys.exit()
-        # else:
-        #    print("invalid Command!")
+            self.withdraw_funds(category=category, amount=amount)
 
     def compute_balance(self, category=""):
         if category == "food":
@@ -337,7 +246,7 @@ class Budget:
 
 
 app = Budget()
-# app.deposit_funds('clothing')
-app.withdraw_funds(category="entertainment", amount=200)
+app.deposit_funds('food')
+#app.withdraw_funds(category="food", amount=100000)
 # app.compute_balance(category='entertainment')
 # app.transfer_balance(category="clothing", to="food")
